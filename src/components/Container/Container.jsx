aa6260f1fc1e8f019be6cf2a51dcb27d9e311709
Evgenii Maiorov <evgenii.lando@gmail.com>
2022-03-23 14:33:43 +0300

    create components

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,13 @@
+import Persons from "../Persons/Persons";
+import classes from "./Container.module.scss";
+
+
+function Container() {
+  return (
+    <div className={classes.Container}>
+      <Persons/>
+    </div>
+  );
+}
+
+export default Container;
